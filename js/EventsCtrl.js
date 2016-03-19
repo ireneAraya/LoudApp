b9f7910b0a8cb92a20b4790de5924bb017f19dc4
Irene Araya <iarayal>
2016-03-19 17:07:42 -0600

    limpiar una función de prueba

--- js/EventsCtrl.js
+++ js/EventsCtrl.js
@@ -3,9 +3,7 @@ angular.module ('loudApp.controllers')
 .controller('EventsCtrl', [
 	'$scope', 'LoudService',
 	function($scope, LoudService) {
-		var Query = LoudService.Query();
-
-		$scope.events = Query('events', '-', 'all');
+		
 
 	}
 ])
