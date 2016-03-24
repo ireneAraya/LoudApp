0c95d17307641a3ff07a3083d5fbc925924bbdf6
Daniel Muñoz <daniel@danielmunoz.cr>
2016-03-24 17:34:36 -0600

    Login validations

--- js/LoginCtrl.js
+++ js/LoginCtrl.js
@@ -14,7 +14,31 @@ angular.module ('loudApp.controllers')
         };
 
         function otherFunctions () {
-            // console.log($scope.data);
+
+            $scope.alerts = [
+                {type: "danger", msg: "Oh snap! There is an error with your login credentials, please review and try again."},
+                {type: "success", msg: "Welcome!"},
+            ];
+
+            $scope.login = function () {
+                var usersCollection = $scope.data.users,
+                    userExists = false;
+
+                var userEmail = $scope.email,
+                    userPass = $scope.password;
+
+                if (userEmail !== "" && userPass !== "") {
+                    for (var i = 0; i < usersCollection.length; i++) {
+                        var user = usersCollection[i];
+
+                        if (userEmail === user.email && userPass === user.password) {
+                            userExists = true;
+                        }
+                    }
+                }
+
+                console.log(userExists);
+            }
         };
 
         $scope.init();
