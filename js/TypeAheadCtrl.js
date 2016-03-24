33e61844da8f2d32f02b86ac5113443f049cb941
nalpizar <natalpizarm@gmail.com>
2016-03-24 17:07:39 -0600

    adding event data to typeAhead

---
+++ js/TypeAheadCtrl.js
@@ -0,0 +1,29 @@
+
+angular.module ('loudApp.controllers')
+
+.controller('TypeaheadCtrl', [
+  '$scope', '$http',
+  function($scope, $http) {
+
+    var _selected;
+    $scope.selected = undefined;
+
+    $scope.ngModelOptionsSelected = function(value) {
+      if (arguments.length) {
+        _selected = value;
+      } else {
+        return _selected;
+      }
+    };
+
+    $scope.modelOptions = {
+      debounce: {
+        default: 500,
+        blur: 250
+      },
+      getterSetter: true
+    };
+
+    $scope.eventName = [{'name':'Iron Maiden'},{'name':'Foo Fighters'},{'name':'Batman VS Super Man'}];
+  }
+])
