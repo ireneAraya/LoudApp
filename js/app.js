6493d24c1c62871dfad196c476b51ce271024ec4
Daniel Muñoz <daniel@danielmunoz.cr>
2016-03-24 16:09:37 -0600

    HEAD

--- js/app.js
+++ js/app.js
@@ -11,15 +11,11 @@ angular.module('loudApp',[
                 templateUrl: 'views/showEvents.html',
                 controller : 'EventsCtrl'
             })
-<<<<<<< HEAD
             .when('/login', {
                 templateUrl: 'views/login.html',
                 controller : 'LoginCtrl'
             })
             .when('/detail/:id', {
-=======
-            .when('/event/:id', {
->>>>>>> c132394e08ada4d65a9a1d64c8e8bad86249456a
                 templateUrl: 'views/eventDetail.html',
                 controller : 'EventsCtrl'
             })
