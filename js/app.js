92126a591f733b927db42a0ada105f110a1d21f3
nalpizar <natalpizarm@gmail.com>
2016-03-24 16:25:50 -0600

    adding typeAhead and ui-bootstrap-angular

--- js/app.js
+++ js/app.js
@@ -2,18 +2,23 @@ angular.module('loudApp',[
     'ngRoute',
     'loudApp.services',
     'loudApp.controllers',
+    'ngAnimate',
+    'ui.bootstrap'
 ])
 
-.config(['$routeProvider', 
+.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider
-            .when('/', {
-                templateUrl: 'views/showEvents.html',
-                controller : 'EventsCtrl'
-            })
+            // .when('/', {
+            //     templateUrl: 'views/showEvents.html',
+            //     controller : 'EventsCtrl'
+            // })
             .when('/detail/:id', {
                 templateUrl: 'views/eventDetail.html',
-                controller : 'EventsCtrl' 
+                controller : 'EventsCtrl'
+            })
+            .when('/', {
+                templateUrl: 'views/buyTickets.html'
             })
             .otherwise({
         	   redirectTo: '/'
