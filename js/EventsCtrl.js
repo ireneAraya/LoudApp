68db50b7f5fc49852cf7b2d93544a0d7aca2e158
Daniel Muñoz <daniel@danielmunoz.cr>
2016-03-24 16:26:57 -0600

    promises fixed

--- js/EventsCtrl.js
+++ js/EventsCtrl.js
@@ -6,20 +6,23 @@ angular.module ('loudApp.controllers')
 
         $scope.init = function() {
             LoudService.getDataFromJS().then(function(response) {
-                $scope.data = response.data;
+                $scope.data = angular.fromJson(response.data);
+                otherFunctions();
             }, function(razon) {
                 $scope.error = razon;
             });
         };
 
-        $scope.getEventLocation = function (index, key) {
-            var location = LoudService.getItem($scope.data.location, index);
-            return location[key];
-        }
+        function otherFunctions () {
+            $scope.getEventLocation = function (index, key) {
+                var location = LoudService.getItem($scope.data.location, index);
+                return location[key];
+            };
 
-        //Lama a la función getItem
-        var currentID = $routeParams.id;
-        // var event = LoudService.getItem($scope.data.events, currentID);
+            //Lama a la función getItem
+            var currentID = $routeParams.id;
+            $scope.event = LoudService.getItem($scope.data.events, currentID);
+        };
 
         $scope.init();
 	}
