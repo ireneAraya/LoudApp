d946776b63c2125dfd9ebb211f219e74ca25b0f3
Daniel Muñoz <daniel@danielmunoz.cr>
2016-03-24 19:05:14 -0600

    changes

--- js/LoginCtrl.js
+++ js/LoginCtrl.js
@@ -39,6 +39,16 @@ angular.module ('loudApp.controllers')
                 }
 
                 console.log(userExists);
+            };
+
+            $scope.facebookLogin = function () {
+                (function(d, s, id){
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) {return;}
+                    js = d.createElement(s); js.id = id;
+                    js.src = "//connect.facebook.net/en_US/sdk.js";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, "script", "facebook-jssdk"));
             }
         };
 
