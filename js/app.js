05df6c0ef2ca09a2f0986bb95e43c4149da73ba9
nalpizar <natalpizarm@gmail.com>
2016-03-25 12:09:02 -0600

    typeAhead working

--- js/app.js
+++ js/app.js
@@ -14,9 +14,9 @@ angular.module('loudApp',[
                 templateUrl: 'views/showEvents.html',
                  controller : 'EventsCtrl'
             })
-            .when('/buy', {
-                templateUrl: 'views/buyTickets.html'
-            })
+            // .when('/', {
+            //     templateUrl: 'views/buyTickets.html'
+            // })
             .when('/login', {
                 templateUrl: 'views/login.html',
                 controller : 'LoginCtrl'
