98e7971c6245d63fb6da30b0ef8d040884d788a5
nalpizar <nalpizar@builderhomesite.com>
2016-04-08 08:52:24 -0600

    tranlation service fix

--- js/LanguageService.js
+++ js/LanguageService.js
@@ -1,13 +1,33 @@
 angular.module ('loudApp.services')
 
 
-.service('translationService', function($resource) {
-
-        this.getTranslation = function($scope, language) {
+.service('translationService', ['$resource', '$q', function($resource, $q) {
+        var getTranslation = function (language) {
+        //     var deferred = $q.defer();
+            language = language || "es";
             var languageFilePath = 'translation_' + language + '.json';
-            console.log(languageFilePath);
-            $resource(languageFilePath).get(function (data) {
-                $scope.translation = data;
-            });
+
+            console.log(language);
+
+        //     $resource(languageFilePath).get(function (data) {
+        //     	deferred.resolve(data);
+        //     });
+
+        //     return deferred.promise;
+	        return $q(function(resolve, reject) {
+			    setTimeout(function() {
+			      // var languageFilePath = 'translation_' + language + '.json';
+
+	            $resource(languageFilePath).get(function (data) {
+	            	resolve(data);
+	            });
+			    }, 1000);
+			  });
         };
-    });
+
+
+        return {
+        	getTranslation : getTranslation
+        }
+    }
+]);
