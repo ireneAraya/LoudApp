0f714781e230dcf692213fd98e8f702e4dac222e
Daniel Muñoz <daniel@danielmunoz.cr>
2016-04-11 17:05:09 -0600

    Minus and currency

--- js/Controllers/buyticketsCtrl.js
+++ js/Controllers/buyticketsCtrl.js
@@ -5,7 +5,7 @@ angular.module ('loudApp.controllers')
     function($scope, $routeParams, $location, LoudService) {
 
         $scope.initialAmount = 0;
-        $scope.itemPrice = 35000;
+        $scope.itemPrice = 3500;
 
         $scope.sumValues = function () {
             $scope.initialAmount += $scope.itemPrice;
@@ -13,6 +13,11 @@ angular.module ('loudApp.controllers')
 
         $scope.differenceValues = function () {
             $scope.initialAmount -= $scope.itemPrice;
+
+            // No deja que de resultados negativos
+            if ($scope.initialAmount <= 0) {
+                $scope.initialAmount = 0;
+            }
         }
 
         $scope.getSelectedValue = function (value) {
