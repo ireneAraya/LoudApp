3f197a3b4a6478f6bd78877315e9f15b65b63b79
Daniel Muñoz <daniel@danielmunoz.cr>
2016-04-17 12:01:17 -0600

    Register Form

--- front-end/js/Controllers/RegisterCtrl.js
+++ front-end/js/Controllers/RegisterCtrl.js
@@ -1,10 +1,12 @@
 angular.module ('loudApp.controllers')
 
 .controller('RegisterCtrl', [
-	'$scope', 'LoudService', 'LoudFB', '$location', '$q', '$rootScope', '$timeout',
-	function($scope, LoudService, LoudFB, $location, $q, $rootScope, $timeout) {
+	'$scope', 'LoudService', '$location', '$q', '$timeout',
+	function($scope, LoudService, $location, $q, $timeout) {
 
         $scope.init = function() {
+            $scope.newImageSource = "/front-end/img/users/profilePlaceholder.png";
+
             var userExists = $q(function (resolve, reject) {
                 var res = LoudService.verifyUser();
 
@@ -22,17 +24,16 @@ angular.module ('loudApp.controllers')
                     otherFunctions();
                 }
             });
-
         };
 
         function otherFunctions () {
-            console.debug("Register!");
+
         };
 
         $scope.init();
 
-        // $scope.$watch('user', function(newValue, oldValue) {
-        //     LoudService.save("LoudApp__User", newValue);
-        // }, true);
+        $scope.$watch('newImageSource', function(newValue, oldValue) {
+            LoudService.save("LoudApp__User", newValue);
+        }, true);
 	}
 ])
