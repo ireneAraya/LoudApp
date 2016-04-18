b5d71637745c17972ae8ecb4b81ca7d03947d260
Leo Picado <leopic@gmail.com>
2016-04-18 08:06:48 -0600

    Probando formulario usando modelos `con puntos`

--- front-end/js/Controllers/RegisterCtrl.js
+++ front-end/js/Controllers/RegisterCtrl.js
@@ -6,6 +6,7 @@ angular.module ('loudApp.controllers')
 
         $scope.init = function() {
             $scope.emailSent = false;
+            $scope.user = {};
 
             var userExists = $q(function (resolve, reject) {
                 var res = LoudService.verifyUser();
@@ -17,7 +18,8 @@ angular.module ('loudApp.controllers')
             });
 
             userExists.then(function (response) {
-                if (response.success) {
+                console.log(response);
+                if (response.success && response.data) {
                     $location.path("/");
                 } else {
                     // Calbacks
@@ -26,56 +28,61 @@ angular.module ('loudApp.controllers')
             });
         };
 
-        function otherFunctions () {
-            $scope.signUp = function () {
-                // Disables the register button while processing
-                $scope.processing = true;
-                $scope.error = null;
+        $scope.signUp = function () {
+            // Disables the register button while processing
+            $scope.processing = true;
+            $scope.error = null;
 
-                // Creates the user object
-                var userToCreate = {
-                    "identification" : $scope.identification,
-                    "identificationType" : $scope.identificationType,
-                    "firstName" : $scope.firstName,
-                    "middleName" : $scope.middleName,
-                    "lastName" : $scope.lastName,
-                    "secondSurname" : $scope.secondSurname,
-                    "nickname" : $scope.nickname,
-                    "email" : $scope.email,
-                    "hash" : $scope.hash,
-                    "verifyPassword" : $scope.verifyPassword,
-                    "birthDate" :  document.getElementById("selectedDate").value,
-                    "phone" : $scope.phone,
-                    "gender" : $scope.gender,
-                    "disability" : $scope.disability,
-                    "specialCondition" : $scope.specialCondition,
-                    "locale" : $scope.locale,
-                    "photoURL" : document.getElementById("userImage").getAttribute("src")
-                };
-
-                // Creates a promise to call the user Service
-                var createUserServiceFunction = $q(function (resolve, reject) {
-                    var res = LoudService.registerUser(userToCreate);
-
-                    $timeout(
-                        function() {
-                            resolve(res)
-                        }, Math.random() * 2000 + 1000);
-                });
-
-                createUserServiceFunction.then(function (response) {
-                    if (!response.success) {
-                        $scope.processing = false;
-                        $scope.error = response.message;
-                    } else {
-                        $scope.processing = false;
-                        $scope.error = null;
-                        $scope.emailSent = true;
-                    }
-                });
+            // Creates the user object
+            var userToCreate = {
+                "identification" : $scope.identification,
+                "identificationType" : $scope.identificationType,
+                "firstName" : $scope.firstName,
+                "middleName" : $scope.middleName,
+                "lastName" : $scope.lastName,
+                "secondSurname" : $scope.secondSurname,
+                "nickname" : $scope.nickname,
+                "email" : $scope.email,
+                "hash" : $scope.hash,
+                "verifyPassword" : $scope.verifyPassword,
+                "birthDate" :  document.getElementById("selectedDate").value,
+                "phone" : $scope.phone,
+                "gender" : $scope.gender,
+                "disability" : $scope.disability,
+                "specialCondition" : $scope.specialCondition,
+                "locale" : $scope.locale,
+                "photoURL" : document.getElementById("userImage").getAttribute("src")
             };
+
+            console.log(userToCreate);
+            console.log($scope.user);
+
+            // Creates a promise to call the user Service
+            var createUserServiceFunction = $q(function (resolve, reject) {
+                var res = LoudService.registerUser(userToCreate);
+
+                $timeout(
+                    function() {
+                        resolve(res)
+                    }, Math.random() * 2000 + 1000);
+            });
+
+            createUserServiceFunction.then(function (response) {
+                if (!response.success) {
+                    $scope.processing = false;
+                    $scope.error = response.message;
+                } else {
+                    $scope.processing = false;
+                    $scope.error = null;
+                    $scope.emailSent = true;
+                }
+            });
         };
+
+        function otherFunctions () {
+            console.log('otherFunctions');
+        }
 
         $scope.init();
 	}
-])
+]);
