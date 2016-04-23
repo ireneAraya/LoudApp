d7bc5e6f6dece5a951b61e5832192a85a9ce7d2c
Daniel Muñoz <daniel@danielmunoz.cr>
2016-04-22 23:07:11 -0600

    location

--- front-end/js/Controllers/LocationsCtrl.js
+++ front-end/js/Controllers/LocationsCtrl.js
@@ -1,8 +1,8 @@
 angular.module ('loudApp.controllers')
 
 .controller('LocationsCtrl', [
-	'$scope', '$routeParams', '$location', 'LoudService', '$timeout', '$q',
-	function($scope, $routeParams, $location, LoudService, $timeout, $q) {
+	'$scope', '$routeParams', '$location', 'LoudService', '$timeout', '$q', '$window',
+	function($scope, $routeParams, $location, LoudService, $timeout, $q, $window) {
 
         $scope.init = function() {
 
@@ -21,9 +21,26 @@ angular.module ('loudApp.controllers')
             getLocations.then(function (response) {
                 if (response && response.data) {
                     $scope.locationsCol = response.data;
-                }    
+                }
 
                 $scope.loadingData = false;
+            });
+        };
+
+        $scope.erraseLocation = function (locationId) {
+            var deleteEvent = $q(function (resolve, reject) {
+                var res = LoudService.deleteItem("locations", locationId);
+
+                $timeout(
+                    function() {
+                        resolve(res)
+                    }, Math.random() * 2000 + 1000);
+            });
+
+            deleteEvent.then(function (response) {
+                if (response && response.success) {
+                    $window.location.reload();
+                }
             });
         };
 
@@ -83,7 +100,7 @@ angular.module ('loudApp.controllers')
         //           $scope.geographicLoc = "";
         //           $scope.address = "";
         //         }
-                
+
         //         $location.path('/locationsList');
         //     }
 
