b0c748aa51d9ff2bc24358129668b5c58ee0bd55
Irene Araya <iarayal>
2016-04-23 00:03:08 -0600

    agregar locaciones

--- front-end/js/Controllers/LocationsCtrl.js
+++ front-end/js/Controllers/LocationsCtrl.js
@@ -27,72 +27,47 @@ angular.module ('loudApp.controllers')
             });
         };
 
+        //Agregar locación
+            $scope.addLocation = function () {
+                // Disables the register button while processing
+                $scope.processing = true;
+                $scope.error = null;
 
-        // function otherFunctions () {
-        //     //$scope.locationsCol = $scope.data.locations;
-
-        //     //Borrar sitio
-        //     $scope.erraselocation = function ($index) {
-        //         var target = LoudService.getItemIndex($scope.locationsCol, $index);
-
-        //         if ($scope.locationsCol.length == 1) {
-        //             $scope.locationsCol = [];
-        //         } else {
-        //             $scope.locationsCol.splice(target, 1);
-        //         }
-
-        //         var parent = document.getElementsByTagName("body")[0],
-        //             child = parent.lastChild;
-
-        //         parent.removeChild(child);
-
-        //         LoudService.save("LoudApp__Locations", $scope.locationsCol);
-
-        //         $location.path('/locationsList');
-        //     }
-
-        //     //Agregar locación
-        //     $scope.addLocation = function () {
-        //         var lastID = 0;
-
-        //         for (var i = 0; i < $scope.locationsCol.length; i++) {
-        //             lastID = (i +1);
-        //         }
-
-        //         //crea el objeto y lo agrega a la colección
-        //         var location = {
-        //             id              : lastID,
-        //             image           : document.getElementById("locationImage").getAttribute("src"),
-        //             name            : $scope.location,
-        //             phone           : $scope.phone,
-        //             capacity        : $scope.capacity,
-        //             geographicLoc   : $scope.geographicLoc,
-        //             address         : $scope.address
-        //         }
-        //         $scope.locationsCol.push(location);
-
-        //         console.table($scope.locationsCol);
-
-        //         // Limpia el formulario, tanto en valores como en estado de variables
-        //         if ($scope.addLocationForm) {
-        //           $scope.addLocationForm.$setPristine();
-        //           $scope.addLocationForm.$setUntouched();
-        //           $scope.location = "";
-        //           $scope.phone = "";
-        //           $scope.capacity = "";
-        //           $scope.geographicLoc = "";
-        //           $scope.address = "";
-        //         }
+                // Creates the object
+                var locationToCreate = {
+                    image           : document.getElementById("locationImage").getAttribute("src"),
+                    name            : $scope.location.name,
+                    contactPhone    : $scope.location.contactPhone,
+                    capacity        : $scope.location.capacity,
+                    geolocation     : $scope.location.geolocation,
+                    address         : $scope.location.address
+                }
                 
-        //         $location.path('/locationsList');
-        //     }
+                $location.path('/locationsList');
+            }
 
-        // };
+                var addItem = $q(function (resolve, reject) {
+                    var res = LoudService.addItem(locationToCreate);
 
-        // $scope.$watch('locationsCol', function(newValue, oldValue) {
-        //     LoudService.save("LoudApp__Locations", newValue);
-        // }, true);
+                    $timeout(
+                        function() {
+                            resolve(res)
+                        }, Math.random() * 2000 + 1000);
+                });
 
-        $scope.init();
+                addItem.then(function (response) {
+                    if (response && response.success) {
+                        $scope.processing = false;
+                        $scope.error = null;
+                        $scope.emailSent = true;
+                    } else {
+                        $scope.processing = false;
+                        $scope.error = response.message;
+                    }
+                });
+            }
+
+            $scope.init();
+        };
 	}
 ])
