77f49106655aa1d67858c9d1d4ec28beb2996f41
Daniel Muñoz <daniel@danielmunoz.cr>
2016-04-22 20:34:54 -0600

    123

--- front-end/js/Controllers/ProfileCtrl.js
+++ front-end/js/Controllers/ProfileCtrl.js
@@ -4,98 +4,93 @@ angular.module ('loudApp.controllers')
 	'$scope', 'LoudService', '$location', '$timeout', 'LoudFB', '$rootScope', '$q',
 	function($scope, LoudService, $location, $timeout, LoudFB, $rootScope, $q) {
 
-        $scope.user = LoudService.verify("LoudApp__User") || {};
-
-        if (isFacebookUser($scope.user)) {
-            $scope.user_fullName = $scope.user.first_name + " " + $scope.user.last_name;
-        } else {
-            $scope.user_fullName = $scope.user.firstName;
-
-            if ($scope.user.middleName !== "") {
-                $scope.user_fullName += " " + $scope.user.middleName;
-            }
-
-            $scope.user_fullName += " " + $scope.user.lastName;
-
-            if ($scope.user.secondSurname !== "") {
-                $scope.user_fullName += " " + $scope.user.secondSurname;
-            }
-        }
 
         $scope.init = function() {
-            LoudService.getDataFromJS().then(function(response) {
-                otherFunctions();
-            }, function(razon) {
-                $scope.error = razon;
-            });
-        };
+            $scope.user = LoudService.verify("LoudApp__User") || {};
 
-
-        function otherFunctions () {
-            // Checks if the user exits
             if (!$scope.user.id) {
                 $location.path('/login');
             }
 
-            // Main system logout function
-            $scope.logout = function () {
-                $scope.endingSession = true;
+            if (isFacebookUser($scope.user)) {
+                $scope.user_fullName = $scope.user.first_name + " " + $scope.user.last_name;
+            } else {
+                $scope.user_fullName = $scope.user.firstName;
 
-                var session = $q(function (resolve, reject) {
-                    var res = LoudService.logoutUser();
+                if ($scope.user.middleName !== "") {
+                    $scope.user_fullName += " " + $scope.user.middleName;
+                }
 
-                    $timeout(
-                        function() {
-                            resolve(res)
-                        }, Math.random() * 2000 + 1000);
-                });
+                $scope.user_fullName += " " + $scope.user.lastName;
 
-                session.then(function (response) {
-                    if (response.success) {
-                        $location.path("/");
-                        $rootScope.$broadcast('userIsLoggedIn', { user : null });
-                    } else {
-                        $scope.error = response.message;
-                    }
-                });
-
+                if ($scope.user.secondSurname !== "") {
+                    $scope.user_fullName += " " + $scope.user.secondSurname;
+                }
             }
-
-            // Facebook logout function
-            $scope.logoutFB = function () {
-                $scope.endingSession = true;
-
-                LoudFB.getLoginStatus().then(function (isLoggedIn) {
-                    if (isLoggedIn) {
-                        LoudFB.logout().then(function (response) {
-
-                            // Removes the user data from localStorage
-                            $scope.user = {};
-                            LoudService.remove("LoudApp__FB_authResponse");
-
-                            // Tells the HeaderCtrl that a user has been disconnected
-                            $rootScope.$broadcast('userIsLoggedIn', { user : null });
-
-                            // Redirects to the Homepage
-                            $location.path("/");
-                        });
-                    }
-                });
-            };
         };
+
+
+        // Main system logout function
+        $scope.logout = function () {
+            $scope.endingSession = true;
+
+            var session = $q(function (resolve, reject) {
+                var res = LoudService.logoutUser();
+
+                $timeout(
+                    function() {
+                        resolve(res)
+                    }, Math.random() * 2000 + 1000);
+            });
+
+            session.then(function (response) {
+                console.log(response);
+
+                if (response && response.success) {
+                    $location.path("/");
+                    $scope.user = {};
+                    $rootScope.$broadcast('userIsLoggedIn', { user : null });
+                    LoudService.remove("LoudApp__User");
+                } else {
+                    $scope.error = response.message;
+                }
+            });
+        };
+
+        // Facebook logout function
+        $scope.logoutFB = function () {
+            $scope.endingSession = true;
+
+            LoudFB.getLoginStatus().then(function (isLoggedIn) {
+                if (isLoggedIn) {
+                    LoudFB.logout().then(function (response) {
+
+                        // Removes the user data from localStorage
+                        $scope.user = {};
+                        LoudService.remove("LoudApp__FB_authResponse");
+
+                        // Tells the HeaderCtrl that a user has been disconnected
+                        $rootScope.$broadcast('userIsLoggedIn', { user : null });
+
+                        // Redirects to the Homepage
+                        $location.path("/");
+                    });
+                }
+            });
+        };
+
+        function isFacebookUser (userData) {
+          var result = false;
+          if (userData && userData.facebook) {
+            result = true;
+          }
+          return result;
+        }
 
         $scope.init();
 
         $scope.$watch('user', function(newValue, oldValue) {
             LoudService.save("LoudApp__User", newValue);
         }, true);
-
-        function isFacebookUser (userData) {
-          var result = false;
-          if (userData.facebook) {
-            result = true;
-          }
-          return result;
-        }
 	}
 ])
