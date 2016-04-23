bf45ce9fd1e8205c5353bb9d43358d9c9beac5df
Daniel Muñoz <daniel@danielmunoz.cr>
2016-04-22 19:24:23 -0600

    asdfghjkl

--- front-end/js/Controllers/LoginCtrl.js
+++ front-end/js/Controllers/LoginCtrl.js
@@ -8,7 +8,6 @@ angular.module ('loudApp.controllers')
 
             $scope.user = {};
 
-
             var userExists = $q(function (resolve, reject) {
                 var res = LoudService.verifyUser();
 
@@ -27,74 +26,75 @@ angular.module ('loudApp.controllers')
 
                     // Tells the HeaderCtrl that a user has logged in a session
                     $rootScope.$broadcast('userIsLoggedIn', { "user" : $scope.user });
-                    $location.path("/");
+
+                    if ($scope.user.rol === 1) {
+                        $location.path("/adminMenu");
+                    } else {
+                        $location.path("/");
+                    }
+
                 } else {
                     $scope.user = {};
                 }
             });
-
-            // Calbacks
-            otherFunctions();
         };
 
-        function otherFunctions () {
-            $scope.login = function () {
-                $scope.logginUser = true;
+        $scope.login = function () {
+            $scope.logginUser = true;
 
-               var callService = $q(function (resolve, reject) {
-                    var res = LoudService.loginUser($scope.email, $scope.password);
+           var callService = $q(function (resolve, reject) {
+                var res = LoudService.loginUser($scope.email, $scope.password);
 
-                    $timeout(
-                        function() {
-                            resolve(res)
-                        }, Math.random() * 2000 + 1000);
-                });
+                $timeout(
+                    function() {
+                        resolve(res)
+                    }, Math.random() * 2000 + 1000);
+            });
 
-                callService.then(function (response) {
-                    if (!response.success) {
-                        $scope.error = response.message;
-                    } else {
-                        var userExists = $q(function (resolve, reject) {
-                            var res = LoudService.verifyUser();
+            callService.then(function (response) {
+                if (!response.success) {
+                    $scope.error = response.message;
+                } else {
+                    var userExists = $q(function (resolve, reject) {
+                        var res = LoudService.verifyUser();
 
-                            $timeout(
-                                function() {
-                                    resolve(res)
-                                }, Math.random() * 2000 + 1000);
-                        });
+                        $timeout(
+                            function() {
+                                resolve(res)
+                            }, Math.random() * 2000 + 1000);
+                    });
 
-                        userExists.then(function (response) {
-                            if (response.success) {
-                                $scope.user = response.data;
-                                $scope.user.facebook = false;
+                    userExists.then(function (response) {
+                        if (response.success) {
+                            $scope.user = response.data;
+                            $scope.user.facebook = false;
 
-                                // Tells the HeaderCtrl that a user has logged in a session
-                                $rootScope.$broadcast('userIsLoggedIn', { "user" : $scope.user });
-                                $location.path("/");
-                            }
-                        });
-                    }
-                });
-            };
+                            // Tells the HeaderCtrl that a user has logged in a session
+                            $rootScope.$broadcast('userIsLoggedIn', { "user" : $scope.user });
+                            $location.path("/");
+                        }
+                    });
+                }
+            });
+        };
 
-            $scope.facebookLogin = function () {
-                $scope.logginUser = true;
-                LoudFB.getLoginStatus().then(function (isLoggedIn) {
-                                                if (!isLoggedIn) {
-                                                    // Logs in the user with Facebook
-                                                    LoudFB.login().then(function (response) {
-                                                        if (response.status === "connected") {
-                                                            // Saves the @authResponse in the localStorage
-                                                            LoudService.save("LoudApp__FB_authResponse", response.authResponse);
+        $scope.facebookLogin = function () {
+            $scope.logginUser = true;
+            LoudFB.getLoginStatus().then(function (isLoggedIn) {
+                                            if (!isLoggedIn) {
+                                                // Logs in the user with Facebook
+                                                LoudFB.login().then(function (response) {
+                                                    if (response.status === "connected") {
+                                                        // Saves the @authResponse in the localStorage
+                                                        LoudService.save("LoudApp__FB_authResponse", response.authResponse);
 
-                                                            getFBUserData($scope);
-                                                        }
-                                                    });
-                                                } else {
-                                                    getFBUserData($scope);
-                                                }
-                                            });
-            };
+                                                        getFBUserData($scope);
+                                                    }
+                                                });
+                                            } else {
+                                                getFBUserData($scope);
+                                            }
+                                        });
         };
 
         function getFBUserData ($scope) {
