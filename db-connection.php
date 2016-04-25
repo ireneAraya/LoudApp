013bc25c9c6f305557cecafbaa115db26ee019a9
Daniel Muñoz <daniel@danielmunoz.cr>
2016-04-25 00:17:05 -0600

    Removing old files

--- db-connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $config = [
-        'db_host' => 'server1.danielmunnoz.com',
-        'db_name' => 'c1LoudApp_MasterDB',
-        'db_user' => 'c1loud',
-        'db_pass' => 'HRqxWBuj5#'
-    ];
- ?>
